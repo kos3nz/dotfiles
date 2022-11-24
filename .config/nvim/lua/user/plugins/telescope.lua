a7626faeccacc5af6fd3c008aa74698e1bcd289a
kos3126 <60677527+kos3126@users.noreply.github.com>
2022-11-24 21:32:00 +0900

    omit hidden option from keymap "find_files"

--- .config/nvim/lua/user/plugins/telescope.lua
+++ .config/nvim/lua/user/plugins/telescope.lua
@@ -122,7 +122,7 @@ telescope.load_extension("media_files")
 local keymap = vim.keymap -- for conciseness
 
 -- file finder
-keymap.set("n", "<leader>ff", "<cmd>Telescope find_files hidden=true<cr>") -- find files within current working directory, respects .gitignore
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
 keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
