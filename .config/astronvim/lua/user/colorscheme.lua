092bd3c8e5f44fbac8e5d46f86a6b755d86d7e75
kos3126 <60677527+kos3126@users.noreply.github.com>
2022-12-28 20:52:56 +0900

    add highlight groups

--- .config/astronvim/lua/user/colorscheme.lua
+++ .config/astronvim/lua/user/colorscheme.lua
@@ -1,10 +1,10 @@
-local colorGrp = vim.api.nvim_create_augroup("UserColors", { clear = true })
-vim.api.nvim_create_autocmd("ColorScheme", {
-  command = [[
-    hi Cursorline guibg=#223137
-  ]],
-  group = colorGrp,
-})
+-- local colorGrp = vim.api.nvim_create_augroup("UserColors", { clear = true })
+-- vim.api.nvim_create_autocmd("ColorScheme", {
+--   command = [[
+--     hi Cursorline guibg=#223137
+--   ]],
+--   group = colorGrp,
+-- })
 
 -- Set colorscheme to use
 return "catppuccin"
