f4006e8db0f5b9274234de4414c63545c71ce97a
kos3126 <60677527+kos3126@users.noreply.github.com>
2023-01-13 16:53:28 +0900

    update markdown linting and formatting settings

--- .config/astronvim/lua/user/mason-null-ls/setup_handlers.lua
+++ .config/astronvim/lua/user/mason-null-ls/setup_handlers.lua
@@ -1,5 +1,4 @@
 return {
-  -- For prettierd:
   prettierd = function()
     require("null-ls").register(require("null-ls").builtins.formatting.prettierd.with({
       filetypes = {
@@ -8,14 +7,14 @@ return {
         "html",
         "javascript",
         "javascriptreact",
-        "json",
         "less",
         "scss",
         "typescript",
         "typescriptreact",
-        "yaml",
-        "toml",
-        -- "markdown",
+        "markdown",
+        -- "json",
+        -- "yaml",
+        -- "toml",
       },
       condition = function(utils)
         return utils.root_has_file("package.json")
@@ -25,7 +24,7 @@ return {
       end,
     }))
   end,
-  -- For eslint_d:
+
   eslint_d = function()
     require("null-ls").register(require("null-ls").builtins.diagnostics.eslint_d.with({
       condition = function(utils)
@@ -33,6 +32,24 @@ return {
           or utils.root_has_file(".eslintrc.json")
           or utils.root_has_file(".eslintrc.js")
           or utils.root_has_file(".eslintrc.cjs")
+      end,
+    }))
+  end,
+
+  markdownlint = function()
+    require("null-ls").register(require("null-ls").builtins.diagnostics.markdownlint)
+    require("null-ls").register(require("null-ls").builtins.formatting.markdownlint.with({
+      condition = function(utils)
+        if -- if any of these files are exist, stop formatting with markdownlint
+          utils.root_has_file("package.json")
+          or utils.root_has_file(".prettierrc")
+          or utils.root_has_file(".prettierrc.json")
+          or utils.root_has_file(".prettierrc.js")
+        then
+          return false
+        end
+
+        return true
       end,
     }))
   end,
