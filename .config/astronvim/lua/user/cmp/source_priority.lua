7fdd6cef1e566e204a6db3cc9248e1a5d1557e98
kos3126 <60677527+kos3126@users.noreply.github.com>
2023-02-22 01:07:43 +0900

    (astronvim/cmp) change source priority for cmp

--- .config/astronvim/lua/user/cmp/source_priority.lua
+++ .config/astronvim/lua/user/cmp/source_priority.lua
@@ -5,9 +5,9 @@
 -- false == disabled
 -- true == 1000
 return {
-  nvim_lsp = 900,
-  nvim_lua = 700,
-  luasnip = 600,
+  luasnip = 800,
+  nvim_lsp = 700,
+  nvim_lua = 600,
   buffer = 500,
   path = 250,
   -- cmp_tabnine = 100,
