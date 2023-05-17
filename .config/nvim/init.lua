cb451749ff99698b832e1d32dd303d314fd0d2a7
kos3126 <60677527+kos3126@users.noreply.github.com>
2023-05-17 10:41:03 +0900

    (nvim) replace nvim directory with astronvim
    - change old nvim directory to nvim.bak

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,23 +1,21 @@
-require("user.plugins-setup") -- should be at the top
-require("user.core.options")
-require("user.core.keymaps")
-require("user.core.colorscheme")
-require("user.plugins.alpha")
-require("user.plugins.autopairs")
-require("user.plugins.bufferline")
-require("user.plugins.colorizer")
-require("user.plugins.comment")
-require("user.plugins.gitsigns")
-require("user.plugins.hlslens")
-require("user.plugins.hop")
-require("user.plugins.illuminate")
-require("user.plugins.impatient")
-require("user.plugins.indentline")
-require("user.plugins.lualine")
-require("user.plugins.lsp")
-require("user.plugins.nvim-cmp")
-require("user.plugins.nvim-tree")
-require("user.plugins.telescope")
-require("user.plugins.toggleterm")
-require("user.plugins.treesitter")
-require("user.plugins.which-key")
+for _, source in ipairs {
+  "astronvim.bootstrap",
+  "astronvim.options",
+  "astronvim.lazy",
+  "astronvim.autocmds",
+  "astronvim.mappings",
+} do
+  local status_ok, fault = pcall(require, source)
+  if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
+end
+
+if astronvim.default_colorscheme then
+  if not pcall(vim.cmd.colorscheme, astronvim.default_colorscheme) then
+    require("astronvim.utils").notify(
+      "Error setting up colorscheme: " .. astronvim.default_colorscheme,
+      vim.log.levels.ERROR
+    )
+  end
+end
+
+require("astronvim.utils").conditional_func(astronvim.user_opts("polish", nil, false), true)
