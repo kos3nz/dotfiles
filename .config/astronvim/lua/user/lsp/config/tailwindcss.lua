0fe4f6098ebd0f4bd1d35bbc37910885cf45a92f
kos3nz <60677527+kos3nz@users.noreply.github.com>
2023-06-07 17:27:56 +0900

    (astronvim) add settings property on tailwindcss lsp

--- .config/astronvim/lua/user/lsp/config/tailwindcss.lua
+++ .config/astronvim/lua/user/lsp/config/tailwindcss.lua
@@ -10,4 +10,32 @@ return {
     "vue",
     "astro",
   },
+  settings = {
+    tailwindCSS = {
+      classAttributes = { "class", "className", "class:list", "classList", "ngClass" },
+      lint = {
+        cssConflict = "warning",
+        invalidApply = "error",
+        invalidConfigPath = "error",
+        invalidScreen = "error",
+        invalidTailwindDirective = "error",
+        invalidVariant = "error",
+        recommendedVariantOrder = "warning",
+      },
+      validate = true,
+      experimental = {
+        classRegex = {
+          {
+            "tw`([^`]*)", -- tw`...`
+            'tw="([^"]*)', -- <div tw="..." />
+            'tw={"([^"}]*)', -- <div tw={"..."} />
+            "tw\\.\\w+`([^`]*)", -- tw.xxx`...`
+            "tw\\(.*?\\)`([^`]*)", -- tw(Component)`...`
+            "cva\\(([^)]*)\\)", -- support for cva
+            "[\"'`]([^\"'`]*).*?[\"'`]",
+          },
+        },
+      },
+    },
+  },
 }
