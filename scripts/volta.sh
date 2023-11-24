3ca723f68b5f1f7a4fb200848bb321d3bace9540
kos3nz <60677527+kos3nz@users.noreply.github.com>
2023-11-24 12:16:50 +0900

    (volta) update script

--- scripts/volta.sh
+++ scripts/volta.sh
@@ -1,11 +1,13 @@
 volta install node
-volta install npm
-volta install yarn
-volta install pnpm
-volta install npm-check-updates
-volta install netlify-cli
-volta install wrangler
-volta install @antfu/ni
-volta install better-commits
-volta install tsx 
-volta install tw3panda-cli 
+volta install npm@latest
+volta install yarn@latest
+volta install pnpm@latest
+volta install npm-check-updates@latest
+volta install @antfu/ni@latest
+volta install @nestjs/cli@latest
+volta install tsx@latest
+volta install ts-node@latest
+# volta install netlify-cli@latest
+# volta install wrangler@latest
+# volta install better-commits@latest
+# volta install tw3panda-cli@latest
