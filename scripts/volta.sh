6eb150993ad01fd1f72d80a44b8a530428a621ef
kos3nz <60677527+kos3nz@users.noreply.github.com>
2023-11-30 15:41:53 +0900

    (volta) add depcheck

--- scripts/volta.sh
+++ scripts/volta.sh
@@ -2,11 +2,13 @@ volta install node
 volta install npm@latest
 volta install yarn@latest
 volta install pnpm@latest
-volta install npm-check-updates@latest
 volta install @antfu/ni@latest
 volta install @nestjs/cli@latest
+volta install depcheck@latest
+volta install npm-check-updates@latest
 volta install tsx@latest
 volta install ts-node@latest
+volta install vercel@latest
 # volta install netlify-cli@latest
 # volta install wrangler@latest
 # volta install better-commits@latest
