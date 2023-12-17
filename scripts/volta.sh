9419ebb48556b97fd574cb055f79e6521e2b9b35
kos3nz <60677527+kos3nz@users.noreply.github.com>
2023-12-17 17:24:22 +0900

    (volta) add dotenv-cli

--- scripts/volta.sh
+++ scripts/volta.sh
@@ -5,6 +5,7 @@ volta install pnpm@latest
 volta install @antfu/ni@latest
 volta install @nestjs/cli@latest
 volta install depcheck@latest
+volta install dotenv-cli@latest
 volta install npm-check-updates@latest
 volta install tsx@latest
 volta install ts-node@latest
