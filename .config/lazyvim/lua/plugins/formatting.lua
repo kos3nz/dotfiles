c8a88358b75024741e214f57c65fc346a67efb7d
kos3nz <60677527+kos3nz@users.noreply.github.com>
2024-05-25 13:08:25 +0900

    (neovim) update formatting.lua

--- .config/lazyvim/lua/plugins/formatting.lua
+++ .config/lazyvim/lua/plugins/formatting.lua
@@ -1,9 +1,13 @@
 return {
   {
     "williamboman/mason.nvim",
-    opts = function(_, opts)
-      table.insert(opts.ensure_installed, "prettierd")
-    end,
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shfmt",
+        "prettierd",
+      },
+    },
   },
   {
     "nvimtools/none-ls.nvim",
@@ -18,23 +22,51 @@ return {
     "stevearc/conform.nvim",
     optional = true,
     opts = {
+      -- LazyVim will use these options when formatting with the conform.nvim formatter
+      format = {
+        timeout_ms = 3000,
+        async = false, -- not recommended to change
+        quiet = false, -- not recommended to change
+        lsp_fallback = true, -- not recommended to change
+      },
+      ---@type table<string, conform.FormatterUnit[]>
       formatters_by_ft = {
-        ["javascript"] = { "prettierd" },
-        ["javascriptreact"] = { "prettierd" },
-        ["typescript"] = { "prettierd" },
-        ["typescriptreact"] = { "prettierd" },
-        ["vue"] = { "prettierd" },
-        ["css"] = { "prettierd" },
-        ["scss"] = { "prettierd" },
-        ["less"] = { "prettierd" },
-        ["html"] = { "prettierd" },
-        ["json"] = { "prettierd" },
-        ["jsonc"] = { "prettierd" },
-        ["yaml"] = { "prettierd" },
-        ["markdown"] = { "prettierd" },
+        javascript = { "prettierd" },
+        javascriptreact = { "prettierd" },
+        typescript = { "prettierd" },
+        typescriptreact = { "prettierd" },
+        vue = { "prettierd" },
+        css = { "prettierd" },
+        scss = { "prettierd" },
+        less = { "prettierd" },
+        html = { "prettierd" },
+        json = { "prettierd" },
+        jsonc = { "prettierd" },
+        yaml = { "prettierd" },
+        markdown = { "prettierd" },
         ["markdown.mdx"] = { "prettierd" },
-        ["graphql"] = { "prettierd" },
-        ["handlebars"] = { "prettierd" },
+        graphql = { "prettierd" },
+        handlebars = { "prettierd" },
+        lua = { "stylua" },
+        fish = { "fish_indent" },
+        sh = { "shfmt" },
+      },
+      -- The options you set here will be merged with the builtin formatters.
+      -- You can also define any custom formatters here.
+      ---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
+      formatters = {
+        injected = { options = { ignore_errors = true } },
+        -- # Example of using dprint only when a dprint.json file is present
+        -- dprint = {
+        --   condition = function(ctx)
+        --     return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
+        --   end,
+        -- },
+        --
+        -- # Example of using shfmt with extra args
+        -- shfmt = {
+        --   prepend_args = { "-i", "2", "-ci" },
+        -- },
       },
     },
   },
