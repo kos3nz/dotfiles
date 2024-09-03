f03f016c089349476129d92ddfc1cbb36bc43d6a
kos3nz <60677527+kos3nz@users.noreply.github.com>
2024-09-03 18:38:38 +0900

    (neovim) update neoscroll config

--- .config/lazyvim/lua/plugins/misc.lua
+++ .config/lazyvim/lua/plugins/misc.lua
@@ -34,17 +34,34 @@ return {
         performance_mode = false, -- Disable "Performance Mode" on all buffers.
       })
 
-      local t = {}
-      -- Syntax: t[keys] = {function, {function arguments}}
-      t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "35" } }
-      t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "35" } }
-      t["<C-b>"] = { "scroll", { "-vim.api.nvim_win_get_height(0)", "true", "70" } }
-      t["<C-f>"] = { "scroll", { "vim.api.nvim_win_get_height(0)", "true", "70" } }
-      t["zt"] = { "zt", { "60" } }
-      t["zz"] = { "zz", { "60" } }
-      t["zb"] = { "zb", { "60" } }
-
-      require("neoscroll.config").set_mappings(t)
+      local neoscroll = require("neoscroll")
+      local keymap = {
+        ["<C-u>"] = function()
+          neoscroll.ctrl_u({ duration = 50 })
+        end,
+        ["<C-d>"] = function()
+          neoscroll.ctrl_d({ duration = 50 })
+        end,
+        ["<C-b>"] = function()
+          neoscroll.ctrl_b({ duration = 100 })
+        end,
+        ["<C-f>"] = function()
+          neoscroll.ctrl_f({ duration = 100 })
+        end,
+        ["zt"] = function()
+          neoscroll.zt({ half_win_duration = 25 })
+        end,
+        ["zz"] = function()
+          neoscroll.zz({ half_win_duration = 25 })
+        end,
+        ["zb"] = function()
+          neoscroll.zb({ half_win_duration = 25 })
+        end,
+      }
+      local modes = { "n", "v", "x" }
+      for key, func in pairs(keymap) do
+        vim.keymap.set(modes, key, func)
+      end
     end,
     event = { "BufReadPre", "BufNewFile" },
   },
