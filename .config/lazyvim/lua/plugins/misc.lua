2d7157fadb0b4da15e1cf1efd7ecd98c19a10e18
kos3nz <60677527+kos3nz@users.noreply.github.com>
2024-09-26 00:31:49 +0900

    (neovim) update settings for Neoscroll

--- .config/lazyvim/lua/plugins/misc.lua
+++ .config/lazyvim/lua/plugins/misc.lua
@@ -28,10 +28,14 @@ return {
         stop_eof = true, -- Stop at <EOF> when scrolling downwards
         respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
         cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-        easing_function = nil, -- Default easing function
+        easing = "linear", -- Easing functions: "linear", "quadratic", "cubic", "quatic", "quintic", "circular", "sine"
         pre_hook = nil, -- Function to run before the scrolling animation starts
         post_hook = nil, -- Function to run after the scrolling animation ends
-        performance_mode = false, -- Disable "Performance Mode" on all buffers.
+        performance_mode = false, -- Performance mode that turns off syntax highlighting while scrolling for slower machines or files with heavy regex syntax highlighting.
+        ignored_events = { -- Events ignored while scrolling
+          "WinScrolled",
+          "CursorMoved",
+        },
       })
 
       local neoscroll = require("neoscroll")
