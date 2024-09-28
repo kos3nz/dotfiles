983940d694878e2df1257f4a994376256ce64ce9
kos3nz <60677527+kos3nz@users.noreply.github.com>
2024-09-28 14:59:21 +0900

    (neovim) update style of LazyVim window

--- .config/lazyvim/lua/config/lazy.lua
+++ .config/lazyvim/lua/config/lazy.lua
@@ -30,7 +30,14 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
+  install = { colorscheme = { "catppuccin" } },
+  ui = {
+    border = "rounded",
+    size = {
+      width = 0.8,
+      height = 0.8,
+    },
+  },
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
