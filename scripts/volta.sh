d9757f869e997d9573ec065391c20557c39e5c1c
kos3nz <60677527+kos3nz@users.noreply.github.com>
2026-01-26 13:22:33 +0900

    (volta) install latest typescript and vtsls language server

--- scripts/volta.sh
+++ scripts/volta.sh
@@ -11,7 +11,9 @@ volta install live-server@latest
 volta install npm-check-updates@latest
 volta install tsx@latest
 volta install ts-node@latest
+volta install typescript@latest
 volta install vercel@latest
+volta install @vtsls/language-server@latest
 # volta install netlify-cli@latest
 # volta install wrangler@latest
 # volta install better-commits@latest
